0016995f1bce74e849a682bb92e71ebd66e83865
20251413 <potato1sgood@gwnu.ac.kr>
2025-09-22 16:21:24 +0900

    09_22

---
+++ AI_C2/Lecture04/Lecture04.c
@@ -0,0 +1,57 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+
+	int index[10] = { 0 };
+	int result[10] = { 0 };
+	int N = 0, i = 0, j = 0;
+	int isRun = 0, isIn = 0;
+
+	while (isRun < 10)
+	{
+		N = rand() % 10;
+		isIn = 0;
+		for (i = 0; i < isRun; i++)
+		{
+			if (N == index[i])
+			{
+				isIn = 1;
+			}
+		}
+		if (!isIn) {
+			index[isRun] = N;
+			isRun++;
+		}
+	}
+
+	for (j = 0; j < 10; j++)
+	{
+		isIn = 0;
+		for (i = 0; i < 10; i++)
+		{
+			if (index[i] == j)
+			{
+				isIn = i;
+				break;
+			}
+		}
+		result[j] = isIn;
+	}
+
+	printf("index : ");
+	for (j = 0; j < 10; j++)
+	{
+		printf("%d ", index[j]);
+	}
+	printf("\nresult : ");
+	for (j = 0; j < 10; j++)
+	{
+		printf("%d ", result[j]);
+	}
+
+	return 0;
+}
---
+++ Lecture03/Lecture03/Lecture03.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <stdio.h>
+
+void sum(int x, int y,int* res)
+{
+    *res = x + y;
+}
+
+int main()
+{
+    int* pa;
+    int a = 3;
+    int b = 2;
+    int c = 0;
+
+    pa = &a;
+
+    sum(a, b, pa);
+
+    printf("%d", *pa);
+
+    return 0;
+}
