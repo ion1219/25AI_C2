7f6aa16a88d63ee017ccd2d8593bab2ebbd0974a
tester <lg970071@gmail.com>
2025-11-03 17:43:16 +0900

    11/03

---
+++ AI_C2/FILE/FILE.cpp
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct student
+{
+    char* name;
+    int atk;
+    int def;
+    int hp;
+};
+
+int main()
+{
+    const char* filename = "students.csv";
+    
+    FILE* fp = NULL;
+    FILE* txt = NULL;
+
+    fopen_s(&fp, filename, "r");
+    fopen_s(&txt, "text.txt", "w");
+
+    if (fp == NULL)
+    {
+        printf("error_1");
+
+        return 1;
+    }
+
+    char buffer[256];
+    char* tokken;
+    char* tokken_ = NULL;
+    student studentInfo[11];
+
+    while (fgets(buffer, sizeof(buffer), fp) != NULL)
+    {
+        int i = 0;
+        tokken = strtok_s(buffer, ",", &tokken_);
+        studentInfo[i].name = tokken;
+        tokken = strtok_s(NULL, ",", &tokken_);
+        studentInfo[i].atk = atoi(tokken);
+        tokken = strtok_s(NULL, ",", &tokken_);
+        studentInfo[i].def = atoi(tokken);
+        tokken = strtok_s(NULL, ",", &tokken_);
+        studentInfo[i].hp = atoi(tokken);
+    }
+
+    int studentNumber;
+    char* studentName = {};
+
+    scanf_s("%d %s", &studentNumber, studentName);
+
+    fclose(fp);
+    fclose(txt);
+
+}
